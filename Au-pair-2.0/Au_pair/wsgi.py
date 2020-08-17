b0ff0b9fd04c14f08b34b49a604881aa9174046f
chriskaringeg <ckaringe@gmail.com>
2020-08-18 01:25:20 +0300

    initiated app

---
+++ Au-pair-2.0/Au_pair/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Au_pair project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Au_pair.settings")
+
+application = get_wsgi_application()
---
+++ Au-pair-2.0/virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chris/Documents/Au-pair-2.0/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
