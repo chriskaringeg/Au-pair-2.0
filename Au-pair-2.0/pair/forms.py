0dfedd9b2f3bfc31be87150f6dcbf6f8d846ea0b
chriskaringeg <ckaringe@gmail.com>
2020-08-19 19:54:51 +0300

    add views and forms

---
+++ Au-pair-2.0/pair/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Image , Profile 
+from django.contrib.auth.models import User
+from pyuploadcare.dj.forms import ImageField
+
+class ImageForm(forms.ModelForm):  
+    class Meta:
+        model = Image
+        fields = ("title","image","description","link","posted_by")
+
+class ProfileForm(forms.Form):
+    bio = forms.CharField(label = "Bio")
+    pic = ImageField(label = "Pic")
